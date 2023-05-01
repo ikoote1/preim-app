b22ac0aa2bb32b35161791f8b20eb88889eb5c28
ikoote1 <ikootepreim@gmail.com>
2023-05-02 01:31:03 +0300

    setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/home" element={<home />} />
+        <Route path="/product" element={<product />} />
+        <Route path="order" element={<order />} />
+      </Routes>
      <h1>ikoote</h1>
     </div>
   );
