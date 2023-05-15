8dba88bb27e55f6e434e315f82522d4b946c5abe
ikoote1 <ikootepreim@gmail.com>
2023-05-16 01:00:44 +0300

    building my app

--- src/components/home.js
+++ src/components/home.js
@@ -1,33 +1,15 @@
 import React from "react";
+import Carousel from 'react-elastic-carousel'
+// import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
+// api = https://fakestoreapi.com/products/
+// api = https://api.escuelajs.co/api/v1/products
 
 const Home = () => {
     return(
-        <div>
-          <div className="heading">
-            <div><p>Let's order</p><p>For your Tummy</p></div>
-            <div>
-                <img className="profile"></img>
-                <img className="menu"></img>
-            </div>
-          </div>
-          <div className="Buttons">
-            <div>
-                <button type="button">Break</button>
-                <button type="button">Lunch</button>
-                <button type="button">Supper</button>
-            </div>
-            <div>
-                <button type="button">Location</button>
-                {/* <input>Search</input> */}
-            </div>
-          </div>
-          <div className="products"></div>
-          <div className="footer">
-            <img></img>
-            <button type="button"> Final order</button>
-            <img></img>
-          </div>
-        </div>
+      <div>
+        <p>preim</p>
+      </div>
+       
     )
 }
 
